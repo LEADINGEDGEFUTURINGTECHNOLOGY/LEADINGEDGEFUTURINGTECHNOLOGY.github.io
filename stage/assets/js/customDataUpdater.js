f71115230f9b8fd1f282922ea0acb67d0b8de457
LEADINGEDGEFUTURINGTECHNOLOGY <77009327+LEADINGEDGEFUTURINGTECHNOLOGY@users.noreply.github.com>
2021-05-13 18:39:48 +0200

    Update customDataUpdater.js

--- stage/assets/js/customDataUpdater.js
+++ stage/assets/js/customDataUpdater.js
@@ -52,7 +52,7 @@ const eventSectionData = [
 
 const AboutUsDetails = {
     'Title': 'About US',
-    'Sub Heading': 'Odio sed id eos et laboriosam consequatur eos earum soluta.',
+    'Sub Heading': 'Educate  Iterate Innovate .',
     'quote': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
     'description': 'Lading id. Sint aliquam consequatur ex ex labore. Et quis qui dolor nulla dolores neque. Aspernatur consectetur omnis numquam quaerat. Sed fugiat nisi. Officiis veniam molestiae. Et vel ut quidem alias veritatis repudiandae ut fugit. Est ut eligendi aspernatur nulla voluptates veniam iusto vel quisquam. Fugit ut maxime incidunt accusantium totam repellendus eum error. Et repudiandae eum iste qui et ut ab alias.',
     points: [
